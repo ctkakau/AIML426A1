a5be2fda75c6b8174b47d3f29e311cb60f8dbb72
ctkakau <chad.kakau@gmail.com>
2024-09-04 23:23:25 +1200

    Q3 finally completed R additional plotting function (ggplot) R additional handling functions (gp solutions) R added renv lock
    py additional python functions

---
+++ accuracies.py
@@ -0,0 +1,200 @@
+# a bunch of functions for getting the best individual
+# in a population; selecting features; modelling and checking accuracies
+
+
+# function to retun the best individual in a population based on fitness
+def best_in_population(pop, fitness = 'fitness'):
+  """determine the best individual in an population based on fitness"""
+  from operator import attrgetter
+  
+  # retrieve from the population, the individual with the best fitness
+  best = max(pop, key = attrgetter(fitness))
+  
+  return best
+
+
+
+# function to extract X based on best individual's feature selection
+def selected_features(best, X):
+  """selects the features from an idividual identified as the best
+  and outputs a reduced dataset using those selected features
+  """
+  
+  import pandas as pd
+  
+  selected = []
+  
+  # create a set of selected features based on the chromosomes of the best individual
+  for f, feature in enumerate(best):
+    
+    # retain the selected features
+    if feature == 1:
+      selected.append(f)
+    
+  # generate the reduced X with just selected features
+  reduced_X = X.iloc[:, selected]
+  
+  return reduced_X
+
+
+
+# function to determine training accuracy of a dataset using DecisionTreeClassifier
+def accuracy_of_selected_features(X, y):
+  """determine the accuracy of a dataset"""
+  from sklearn.tree import DecisionTreeClassifier
+  from sklearn.model_selection import train_test_split
+  
+  # split data for training and testing
+  X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.4)
+  
+  # run the classifier
+  clf = DecisionTreeClassifier()
+  clf = clf.fit(X_train, y_train)
+  
+  accuracy = clf.score(X_test, y_test)
+  
+  return accuracy
+
+
+# function for running experiment N-times
+def multi_run(func, data = None, feat_names = None, params = None, n=1, seed = None):
+    """create multiple runs of a selected experiment"""
+    from datetime import datetime as dt
+    # accept only singular parameters - i.e. feed one function only
+    hallOfFame = []
+    run_stats = []
+    time_delta = []
+    population = []
+
+    for r in range(n): 
+        start = dt.now()
+        if feat_names:
+            if params: # feat_sel_GA
+                pop, stat, hof = func(data, feat_names, params)
+                
+            else: # knapsack
+                pop, stat, hof = func(data, feat_names)
+                
+        else: #symbreg
+            pop, stat, hof = func(seed = r)
+
+        
+        end = dt.now()
+        time = end - start
+
+        run_stats.append(stat)
+        hallOfFame.append(hof)
+        time_delta.append(time)
+        population.append(pop)
+                
+    return population, run_stats, hallOfFame, time_delta, 
+
+
+# function for running through the different datasets or different functions
+def changing_run(paths = None, feat_names = None, func = None, params = None, n = 1):
+  """run an experiment over different datasets"""
+
+  diffs = [[], [], [], []] 
+
+  if len(paths) > 1:
+      # iterate paths, keep all other parameters the same, function is required, params are optional
+      for p, dat in enumerate(paths):
+          if len(feat_names) > 1:
+              # include feat_names input
+              if params:
+                  run, best, time, population = multi_run(func, 
+                                         data= dat, 
+                                         feat_names= feat_names[p], 
+                                         params = params, 
+                                         n = n)
+              else:
+                  run, best, time, population = multi_run(func, 
+                                         data= dat, 
+                                         feat_names= feat_names[p], 
+                                         n = n)
+          
+          else:
+              
+              if params:
+                  run, best, time, population = multi_run(func, 
+                                         data= dat, 
+                                         params = params, 
+                                         n = n)
+              else:
+                  run, best, time, population = multi_run(func, 
+                                         data= dat, 
+                                         n = n)
+          diffs[0].append(run)
+          diffs[1].append(best)
+          diffs[2].append(time)
+          diffs[3].append(population)
+
+  return diffs
+
+
+# function to convert results into a table
+def results_table(populations):
+  import pandas as pd
+  import numpy as np
+  import deap
+  
+  best_indivs = [best_in_population(pop) for pop in populations]
+  
+  fitness = [round(indiv.fitness.values[0], 3) for indiv in best_indivs]
+  programme_size = [len(indiv) for indiv in best_indivs]
+  
+  stats = pd.DataFrame({'fitness' : [np.mean(fitness), np.std(fitness)] , 'programme size': [np.mean(programme_size), np.std(programme_size)]}, index = ['average', 'std_dev'])
+  results = pd.DataFrame({'fitness' : fitness, 'programme size': programme_size}, index = ['run'+str(i+1) for i in range(len(fitness))])
+  
+  results = pd.concat([results, stats])
+
+  return results
+
+
+
+# use networkx to draw a tree of a gp individual
+def draw_tree(indiv):
+  from deap import gp
+  import networkx as nx
+  import matplotlib.pyplot as plt
+  
+  # use deap.gp.graph function to collect tree info
+  nodes, edges, labels = gp.graph(indiv)
+  
+  g = nx.Graph()
+  
+  g.add_nodes_from(nodes)
+  g.add_edges_from(edges)
+  pos = nx.nx_agraph.graphviz_layout(g, prog = 'dot')
+  
+  # will this help with over-writes?
+  # H = nx.convert_node_labels_to_integers(g, label_attribute="node_label")
+  # 
+  # H_layout = nx.nx_agraph.pygraphviz_layout(g, prog="dot")
+  # 
+  # G_layout = {H.nodes[n]["node_label"]: p for n, p in H_layout.items()}
+
+  nx.draw_networkx_nodes(g, pos, node_size = 300)
+  nx.draw_networkx_edges(g, pos)
+  nx.draw_networkx_labels(g, pos, labels)
+  
+  plt.show()
+
+
+# function to draw a tree using pygraphviz
+def draw_pgv_tree(indiv, path = None):
+  from deap import gp
+  import pygraphviz as pgv
+  
+  nodes, edges, labels = gp.graph(indiv)
+
+  g = pgv.AGraph()
+  g.add_nodes_from(nodes)
+  g.add_edges_from(edges)
+  g.layout(prog = 'dot')
+  
+  for i in nodes:
+    n = g.get_node(i)
+    n.attr['label'] = labels[i]
+    
+  g.draw() if path is None else g.draw(path)
--- symbreg.py
+++ symbreg.py
@@ -28,27 +28,54 @@ from deap import tools
 from deap import gp
 
 # Define new functions
-def protectedDiv(left, right):
+def _div_(left, right):
     try:
         return left / right
     except ZeroDivisionError:
         return 1
       
+def _if_(input, output1, output2):
+  return output1 if input else output2
 
-pset = gp.PrimitiveSet("MAIN", 1)
-pset.addPrimitive(operator.add, 2)
-pset.addPrimitive(operator.sub, 2)
-pset.addPrimitive(operator.mul, 2)
-pset.addPrimitive(protectedDiv, 2)
-pset.addPrimitive(operator.neg, 1)
-pset.addPrimitive(math.cos, 1)
-pset.addPrimitive(math.sin, 1)
-#pset.addPrimitive(math.sqrt, 1) # add sqrt
-# pset.addPrimitive(numpy.power, 2) # add power
-pset.addEphemeralConstant("rand101", partial(random.randint, -1, 1))
+pset = gp.PrimitiveSetTyped("MAIN", [float,], float)
+# boolean primitives
+pset.addPrimitive(operator.xor, [bool, bool], bool) 
+pset.addPrimitive(operator.and_, [bool, bool], bool)
+pset.addPrimitive(operator.or_, [bool, bool], bool)
+pset.addPrimitive(operator.not_, [bool], bool)
+
+
+# boolean float primitives
+pset.addPrimitive(_if_, [bool, float, float], float)
+pset.addPrimitive(operator.le, [float, float], bool)
+pset.addPrimitive(operator.ge, [float, float], bool)
+
+
+# float primitives
+pset.addPrimitive(operator.add, [float, float], float)
+pset.addPrimitive(operator.sub, [float, float], float)
+pset.addPrimitive(operator.mul, [float, float], float)
+pset.addPrimitive(_div_, [float, float], float)
+pset.addPrimitive(operator.neg, [float], float)
+pset.addPrimitive(math.cos, [float], float)
+pset.addPrimitive(math.sin, [float], float)
+
+pset.addEphemeralConstant('rand101', partial(random.choice, [-1.0, 0.0,  1.0] ), float) 
+
+# float and boolean terminals
+# pset.addTerminal(0.0, float)
+pset.addTerminal(1.0, float) # add float terminal
+pset.addTerminal(2.0, float) # add float terminal
+pset.addTerminal(3.0, float)
+pset.addTerminal(5.0, float)
+pset.addTerminal(7.0, float)
+pset.addTerminal(1, bool)
+# pset.addTerminal(0, bool)
+
 pset.renameArguments(ARG0='x')
 
-creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
+
+creator.create("FitnessMin", base.Fitness, weights=(-1.0, ))
 creator.create("Individual", gp.PrimitiveTree, fitness=creator.FitnessMin)
 
 toolbox = base.Toolbox()
@@ -57,36 +84,27 @@ toolbox.register("individual", tools.initIterate, creator.Individual, toolbox.ex
 toolbox.register("population", tools.initRepeat, list, toolbox.individual)
 toolbox.register("compile", gp.compile, pset=pset)
 
-# def trainingdata():
-#   X = [numpy.arange(-10, 10, step = 0.1)]
-#   y = [(1/x + np.sin(x)) if (x > 0).any() else (2*x + x**2 + 3.0) for x in X ]
 
 def evalSymbReg(individual, points):
     # Transform the tree expression in a callable function
     func = toolbox.compile(expr=individual)
     # Evaluate the mean squared error between the expression
-    # and the real function : x**4 + x**3 + x**2 + x
-    #### I don't know how to evaluate without including the target functions
     # f(x) = 1/x +sinx, for x>o ; 2x+x**2 + 3.0, for x <= 0
-    #### is it just x reference values and corresponding y values?
-    #### or do we just plug it in and assume it's harder generate a single
-    #### function that does both
-    # sqerrors = ((func(x) - x**4 - x**3 - x**2 - x)**2 for x in points)
-    sqerrors = ((func(x) - 1/x - math.sin(x)) if (x>0) else (func(x)- 2*x - x**2 - 3.0) for x in points)
+    sqerrors = ((func(x) - 1/x - math.sin(x))**2 if (x>0) else (func(x)- 2*x - x**2 - 3.0)**2 for x in points)
     
     return math.fsum(sqerrors) / len(points),
 
-toolbox.register("evaluate", evalSymbReg, points=[x/10. for x in range(-10,10)])
+toolbox.register("evaluate", evalSymbReg, points=[x/10. for x in range(-3,3)])
 toolbox.register("select", tools.selTournament, tournsize=3)
-toolbox.register("mate", gp.cxOnePoint)
-toolbox.register("expr_mut", gp.genFull, min_=0, max_=2)
-toolbox.register("mutate", gp.mutUniform, expr=toolbox.expr_mut, pset=pset)
+toolbox.register("mate", gp.cxOnePoint,) #LeafBiased, termpb = 0.3,) # prob of selecting terminal
+toolbox.register("expr_mut", gp.genFull, min_=1, max_=2)
+toolbox.register("mutate", gp.mutUniform, expr=toolbox.expr_mut, pset=pset) # from mutUniform
 
-toolbox.decorate("mate", gp.staticLimit(key=operator.attrgetter("height"), max_value=6)) # 17 these trees are ridiculous
-toolbox.decorate("mutate", gp.staticLimit(key=operator.attrgetter("height"), max_value=6))
+toolbox.decorate("mate", gp.staticLimit(key=operator.attrgetter("height"), max_value=5)) # 17 these trees are ridiculous
+toolbox.decorate("mutate", gp.staticLimit(key=operator.attrgetter("height"), max_value=5))
 
-def main():
-    # random.seed(318)
+def main(seed = 318):
+    random.seed(seed)
 
     pop = toolbox.population(n=300)
     hof = tools.HallOfFame(1)
